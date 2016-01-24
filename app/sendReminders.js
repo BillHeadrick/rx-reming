958cf38d86f698717d1cdb43df234046073d192a
William Headrick <headrick@utexas.edu>
2016-01-24 16:22:29 -0600

    added worker

--- app/sendReminders.js
+++ app/sendReminders.js
@@ -2,28 +2,32 @@
  * Created by William on 1/24/2016.
  * This function will search for all users who have a scheduled time past the current minimum and then send them an email
  */
+function master() {
+    var mongoose = require('mongoose');  // mongoose for mongodb
+    var twilio = require('twilio');
+    var client = new twilio.RestClient('AC3e46165d0c7eaf567cf553ebf7fa563b', '4b011dadd9dcd4f278da18347242161c');
 
-var mongoose = require('mongoose');  // mongoose for mongodb
-var twilio = require('twilio');
-var client = new twilio.RestClient('AC3e46165d0c7eaf567cf553ebf7fa563b', '4b011dadd9dcd4f278da18347242161c');
+    mongoose.connect('process.env.MONGOLAB_URI', function (error) {
+        if (error) {
+            console.error(error)
+        }
+        else console.log('mongo connected');
+    });
+    var date = new Date();
+    var Reminder = require('./models/Reminders.js');
 
-mongoose.connect('process.env.MONGOLAB_URI', function(error){
-    if(error){console.error(error)}
-    else console.log('mongo connected');
-});
-var date = new Date();
-var Reminder = require('./models/Reminders.js');
-
-Reminder.find({alarms: {$lte: date}}, function(err, reminders){
-    if(err){console.log(err)}
-    else {
-        console.log(reminders);
-        reminders.forEach(function(r){
-            sendReminder(client, r);
-        })
-    }
-});
-
+    Reminder.find({alarms: {$lte: date}}, function (err, reminders) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log(reminders);
+            reminders.forEach(function (r) {
+                sendReminder(client, r);
+            })
+        }
+    });
+}
 sendReminder = function(client, r){
     client.sms.messages.create({
         to: r.phone,
@@ -39,8 +43,5 @@ sendReminder = function(client, r){
         }
     });
 }
-/* This is a function that searches for all alarms in the alarms[] that satisfy the date
-Reminder.find({alarms : 'Tue Jan 26 2016 01:00:00 GMT-0600 (Central Standard Time)'},function(err, reminders){
-    console.log(reminders);
-})
-*/
+
+master();
